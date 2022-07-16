80ab8a7ff3e0852793ad60d08dab15914222661d
theLastKing711 <lastking711@protonmail.com>
2022-07-16 05:34:33 +0300

    fix cors

--- E-commerce-API/Program.cs
+++ E-commerce-API/Program.cs
@@ -90,10 +90,9 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.UseStaticFiles();
-
 app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
 
+app.UseStaticFiles();
 
 app.UseAuthentication();
 
